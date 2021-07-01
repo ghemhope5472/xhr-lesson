1296dbe2fe24fa2b1f7d21de0144420b53bb78f7
Glenn <ghemhope@gmail.com>
2021-07-01 13:08:13 +0800

    a

---
+++ index.js
@@ -0,0 +1,17 @@
+var XHR  = new XMLHttpRequest();
+
+
+
+XHR.onreadystatechange = function(){
+    if( XHR.readyState === 4 ){
+        if( XHR.status === 200){
+            console.log(XHR.responseText)
+        } else {
+            console.log('Error Not found')
+        }
+    }
+
+}
+
+XHR.open('GET', 'https://api.github.com/zen')
+XHR.send()
